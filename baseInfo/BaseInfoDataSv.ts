3dc2fc2a8c4e22b5f4fb059dd443213b698fe2ab
guozhaokui <guozk@layabox.com>
2016-01-14 20:00:12 +0800

    ..

--- baseInfo/BaseInfoDataSv.ts
+++ baseInfo/BaseInfoDataSv.ts
@@ -1,13 +1,16 @@
 ///<reference path="e:/tsdefination/node/node.d.ts" />
 'use strict';
+/**
+ * 建立一个服务器，给调试器客户端提供一些信息。
+ */
 import * as net from "net";
-var baseinfoSvPort=9981;
-var log=console.log;
-class BaseInfoDataSv{
+var baseinfoSvPort = 9981;
+var log = console.log;
+class BaseInfoDataSv {
     _socket = net.createServer();
-    _client:net.Socket=null;    //client
-    constructor(){
-        
+    _client: net.Socket = null;    //client
+    constructor() {
+
     }
     
     //启动服务
@@ -21,7 +24,7 @@ class BaseInfoDataSv{
         this._socket.on('timeout', this.onTimeout.bind(this));
         log('net server listen on' + baseinfoSvPort);
     }
-    
+
     onListened() {
         log('onListened');
     }
@@ -58,60 +61,162 @@ class BaseInfoDataSv{
         this._client = c;
         c.on('data', this.onData.bind(this));
     }
-    
-    send(data:Buffer|string){
-        if(this._client)
+
+    send(data: Buffer | string) {
+        if (this._client)
             this._client.write(data);
     }
 }
 
-export class anotherAgent{
-    _getConnectData:boolean=false;
-    _sv=new BaseInfoDataSv();
-    _runtimeVersion:string;
-    _recvData:string='';
-    constructor(){
+
+/**
+ * 类似v8/http的网络协议。
+ * header是一个以两个\r\n结束的数据
+ * 例如
+ * ....\r\n
+ * Content-Length: len\r\n\r\n
+ * 注意：
+ * 这个是主文件，不要维护其他版本的
+ */
+class v8txtprot {
+    static contentlenStr = 'Content-Length: ';
+    _data = '';
+    /**
+     * 0 head
+     * 1 body
+     * 2 error
+     */
+    _state: number = 0;    
+    /**
+     * 接收到完整消息的处理.
+     * @param prot {string} 一个完整的消息。
+     */
+    _onProt: (prot: string) => void;
+    /**
+     * 协议解析错误。
+     */
+    _onErr: (msg: string) => void;
+    _onHeader: (head: any) => void;
+    /**
+     * 协议正文的期望长度
+     */
+    _needLen = 0;
+    _header: any = null;
+    constructor() { }
+    wrapSendMsg(msg: string): string {
+        var ret = v8txtprot.contentlenStr;
+        ret += msg.length;
+        ret += '\r\n\r\n';
+        ret += msg;
+        return ret;
+    }
+    /**
+     * 解析header
+     */
+    handleHeader(dt: string): any {
+        var ret = {};
+        var lines = dt.split('\r\n');
+        lines.forEach((v, i, arr) => {
+            var d = v.split(': ');
+            ret[d[0]] = d.length > 0 ? d[1] : null;
+        });
+        return ret;
+    }
+    parsedata() {
+        while (true) {
+            if (this._state == 0) {
+                var pos = this._data.indexOf('\r\n\r\n');
+                if (pos >= 0) {
+                    var header = this._data.substr(0, pos);
+                    this._header = this.handleHeader(header);
+                    this._onHeader && this._onHeader(this._header);
+                    this._needLen = parseInt(this._header['Content-Length']);
+                    if (this._needLen <= 0)
+                        this._state = 0;  //继续head状态
+                    else if (isNaN(this._needLen)) {
+                        this._onErr && this._onErr('content lenth error!' + header);
+                        this._state = 3;
+                    } else {
+                        this._state = 1;
+                    }
+                    this._data = this._data.substr(pos + 4);
+                } else {
+                    break;//终止while
+                }
+            }
+            else if (this._state == 1) {
+                if (this._data.length >= this._needLen) {
+                    this._onProt && this._onProt(this._data.substr(0, this._needLen));
+                    this._data = this._data.substr(this._needLen);
+                    this._state = 0;
+                } else {
+                    break;//终止while
+                }
+            } else {
+                //err 不知道怎么处理
+                this._data = '';
+                this._state = 0;
+                break;
+            }
+        }
+    }
+    onData(dt: string) {
+        this._data += dt;
+        this.parsedata();
+    }
+}
+
+/**
+ * NI的网络消息也要发给这个对象，这个对象在分析后，传给调试器客户端
+ * 
+ */
+export class anotherAgent {
+    _firstHeader = true;
+    _sv = new BaseInfoDataSv();
+    _prot = new v8txtprot();
+    _runtimeVersion: string;
+    constructor() {
+        this._prot._onProt = this.handleProtocol.bind(this);
+        this._prot._onErr = this.handleProtError.bind(this);
+        this._prot._onHeader = this.handleHeader.bind(this);
         this._sv.start();
     }
-    onConnect(){
-        
+    onConnect() {
+        //NI与runtime建立连接了
+        this._sv.send('');
     }
-    onClose(){
-        
+    onClose() {
+        //NI与runtime断开了
     }
-    onEnd(){
-        
+    onEnd() {
+        //NI与runtime断开了
     }
-    onError(e:Error){
-        
+    onError(e: Error) {
+        //NI与runtime断开了
     }
-    onData(dt:Buffer){
+    onData(dt: Buffer) {
         //console.log('xxxxx data:'+dt);
-        this._recvData+=dt.toString();
-        if( !this._getConnectData){
-            var pos =this._recvData.indexOf('\r\n\r\n'); 
-            if( pos>0){
-                this.handleConnectData(this._recvData.substr(0,pos));
-                this._recvData=this._recvData.substr(pos+4);
-                this._getConnectData=true;
-            }
-        }else{
-            
-        }
-        //this._sv.send(dt);
+        this._prot.onData(dt.toString());
     }
-    handleConnectData(dt:string){
-        var result = dt.match(/layabox version\: ([^\r]*)/);
-        if(result.length>=2){
-            this._runtimeVersion=result[1];
-        }else
-            this._runtimeVersion='NA';
-        this._sv.send(`{"rver":"${this._runtimeVersion}"}`);
+
+    send2C(msg: string) {
+        this._sv.send(this._prot.wrapSendMsg(msg));
+    }
+    handleHeader(head: any) {
+        if (this._firstHeader) {
+            var layaver = head['layabox version'];
+            layaver = layaver ? layaver : 'NA';
+            this.send2C(`{"type":"baseinfo","rver":"${layaver}"}`);
+            this._firstHeader = false;
+        }
+    }
+    handleProtError(dt: string) {
+        console.log(dt);
     }
     /**
      * @param msg 是一个完整的协议
      */
-    handleProtocol(msg:string){
-        
+    handleProtocol(msg: string) {
+
     }
 }
